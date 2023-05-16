985aa8faaf6c1eb91a980e20672b9a3402124002
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-05-15 23:47:35 -0300

    Url update

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -8,27 +8,27 @@ import { Educacion } from '../model/educacion';
 })
 export class EducacionService {
 
-  expURL = 'http://localhost:8080/explab';;
+  URL = 'https://backendmlm.onrender.com/explab';;
 
   constructor(private httpClient : HttpClient) { }
 
   public lista(): Observable<Educacion[]>{
-    return this.httpClient.get<Educacion[]>(this.expURL + 'lista');
+    return this.httpClient.get<Educacion[]>(this.URL + 'lista');
   }
 
   public detail(id: number): Observable<Educacion>{
-    return this.httpClient.get<Educacion>(this.expURL + `detail/${id}`);
+    return this.httpClient.get<Educacion>(this.URL + `detail/${id}`);
   }
 
   public save(educacion: Educacion): Observable<any>{
-    return this.httpClient.post<any>(this.expURL + 'create', educacion);
+    return this.httpClient.post<any>(this.URL + 'create', educacion);
   }
 
   public update(id: number, educacion: Educacion): Observable<any>{
-    return this.httpClient.put<any>(this.expURL + `update/${id}`, educacion);
+    return this.httpClient.put<any>(this.URL + `update/${id}`, educacion);
   }
 
   public delete(id: number): Observable<any>{
-    return this.httpClient.delete<any>(this.expURL + `delete/${id}`);
+    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
   }
 }
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -10,7 +10,7 @@ import {persona} from '../model/persona.model'
 
 export class PersonaService {
 
-URL = 'http://localhost:8080/personas/';
+URL = 'https://backendmlm.onrender.com/';
 
   constructor(private http: HttpClient) { }
 
