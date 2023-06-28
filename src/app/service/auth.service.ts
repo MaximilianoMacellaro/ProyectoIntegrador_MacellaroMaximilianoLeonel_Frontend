cb23bf1fee42699fd2aa43f266c4e033dd7c441f
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-06-28 11:23:51 -0300

    Auth login arreglo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,7 +10,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
 })
 export class AuthService {
 
-  authURL = 'https://localhost:8080/auth/';
+  authURL = 'https://backendmlm.onrender.com/auth';
 
   constructor(private httpClient: HttpClient) { }
 
