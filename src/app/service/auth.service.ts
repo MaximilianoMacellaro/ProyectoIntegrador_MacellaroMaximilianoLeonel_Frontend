0debac18c3bc61947cb161118bbf13c9de992755
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-06-28 11:57:36 -0300

    Firebase

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,7 +10,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
 })
 export class AuthService {
 
-  authURL = 'https://backendmlm.onrender.com/auth/';
+  authURL = 'https://backendmlm.onrender.com/auth';
 
   constructor(private httpClient: HttpClient) { }
 
