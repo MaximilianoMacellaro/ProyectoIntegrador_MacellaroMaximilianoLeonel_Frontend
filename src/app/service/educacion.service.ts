16626303a52f154f489cbde928cb4beaedb35c0d
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-06-29 14:43:48 -0300

    Arreglo skills

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -17,18 +17,18 @@ export class EducacionService {
   }
 
   public detail(id: number): Observable<Educacion>{
-    return this.httpClient.get<Educacion>(this.URL + `detail/${id}`);
+    return this.httpClient.get<Educacion>(this.URL + `/detail/${id}`);
   }
 
   public save(educacion: Educacion): Observable<any>{
-    return this.httpClient.post<any>(this.URL + 'create', educacion);
+    return this.httpClient.post<any>(this.URL + '/create', educacion);
   }
 
   public update(id: number, educacion: Educacion): Observable<any>{
-    return this.httpClient.put<any>(this.URL + `update/${id}`, educacion);
+    return this.httpClient.put<any>(this.URL + `/update/${id}`, educacion);
   }
 
   public delete(id: number): Observable<any>{
-    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+    return this.httpClient.delete<any>(this.URL + `/delete/${id}`);
   }
 }
